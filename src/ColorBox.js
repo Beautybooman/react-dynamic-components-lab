bc101f8acb918db931dbb8779940d28d4e93bc6b
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-06-03 21:12:03 -0400

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -10,8 +10,13 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: 0.2 /*replace null with the value*/}}>
+        {this.props.opacity}
+        { if (opacity = 0.2) {
+          
+        } 
+        
+        }
       </div>
     )
   }
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,19 @@
-//your code here
+import React, { Component } from 'react'
+
+    // export default class BlogPost extends Component {
+class Comment extends Component {
+    render (){
+     
+        return (
+        <div className="comment"> 
+        <p>{this.props.commentText} </p>
+    
+
+         </div>
+
+    )
+}
+}
+
+export default Comment
+
